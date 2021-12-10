a753110a4f7f7863fa123b6c2d10583b2054daa1
Viktor Rudenko <v.rudenko@triacom.ua>
2021-12-10 14:25:34 +0200

    Some Fixesgit add .!

--- auctions/models.py
+++ auctions/models.py
@@ -21,7 +21,7 @@ class Lot(models.Model):
     title = models.CharField(max_length=64)
     description  = models.TextField(max_length=512)
     first_bet = models.IntegerField()
-    default_bet = models.IntegerField()
+    default_bet = models.IntegerField(default=1, blank=True)
     photo = models.URLField(max_length=200, blank=True)
     category = models.ManyToManyField(Category, related_name="lots")
     author = models.ForeignKey(User, default="1", on_delete=CASCADE, related_name="lot")
